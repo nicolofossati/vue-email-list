8fac7c0b2271507f78469105e49d47c05d6a59ae
nicolo fossati <nicofossa01@gmail.com>
2023-03-23 14:42:29 +0100

    ESERCIZIO BASE CONSCLUSO

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        mail: ""
+        currentMail : "",
+        mails: []
       }
     },
+    methods :{
+        addTenMailToMails(){
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response)=>{
+                    this.currentMail = response.data.response;
+                    this.mails.push(this.currentMail);
+                });
+            }
+        }
+    },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response)=>{
-            this.mail = response.data.response;
-        });
+        this.addTenMailToMails();
     } 
   }).mount('#app')
