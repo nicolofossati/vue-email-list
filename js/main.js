6fbd9fb88108981d9fa5bbb720b546489be78c8c
nicolo fossati <nicofossa01@gmail.com>
2023-03-23 14:45:55 +0100

    eliminazione codice rindondante

--- js/main.js
+++ js/main.js
@@ -12,8 +12,7 @@ const { createApp } = Vue
             for(let i=0; i<10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response)=>{
-                    this.currentMail = response.data.response;
-                    this.mails.push(this.currentMail);
+                    this.mails.push(response.data.response);
                 });
             }
         }
