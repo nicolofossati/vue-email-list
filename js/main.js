3dfb2ad632e1d016176aae5d3815cc441232d7f7
nicolo fossati <nicofossa01@gmail.com>
2023-03-23 14:52:27 +0100

    BONUS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        currentMail : "",
+        currentMail: "",
+        isReady: false,
         mails: []
       }
     },
@@ -13,6 +14,9 @@ const { createApp } = Vue
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response)=>{
                     this.mails.push(response.data.response);
+                    if(i==9){
+                        this.isReady = true;
+                    }
                 });
             }
         }
